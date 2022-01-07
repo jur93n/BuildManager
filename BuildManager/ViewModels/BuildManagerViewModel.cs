4d9617024f064eada6492d4e4fdfc9a5cee9181e
yurii_oliinyk <oliynyk.yu@gmail.com>
2022-01-07 22:46:52 +0200

    Create ViewModel for Build Manager

---
+++ BuildManager/ViewModels/BuildManagerViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using BuildManager.Models;
+
+namespace BuildManager.ViewModels
+{
+    public class BuildManagerViewModel : INotifyPropertyChanged
+    {
+        private BuildManagerModel _selectedItem;
+
+        public ObservableCollection<BuildManagerModel> ManagerModels { get; set; }
+        public BuildManagerModel SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged("SelectedItem");
+            }
+        }
+
+        public BuildManagerViewModel()
+        {
+            ManagerModels = new ObservableCollection<BuildManagerModel>
+            {
+                new BuildManagerModel { TypeOfWork="Зламати стіну", Worker="Міхалич", Duration="Два дні", TotalPrice=200}
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
