23bcfeabbea487e5f89290bc9400745b0c04ecba
yurii_oliinyk <oliynyk.yu@gmail.com>
2022-01-10 13:14:04 +0200

    DataGrid to ListView Done

---
+++ BuildManager/Views/AddNewWorkWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace BuildManager.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AddNewWorkWindow.xaml
+    /// </summary>
+    public partial class AddNewWorkWindow : Window
+    {
+        public AddNewWorkWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
